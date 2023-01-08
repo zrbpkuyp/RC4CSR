565d7c319eb748c8a1fbd9b596aff11e32827fb5
Zhao Haochen <jtnkc@icloud.com>
2023-01-08 18:22:02 +0800

    system: init Writing sector

--- System/Writing/models.py
+++ System/Writing/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+__all__ = [
+    'Pencraft',
+]
+
+class Pencraft(models.Model):
+    """Pencraft
+
+    :param models: To be completed
+    :type models: To be completed
+    """
+    topic = models.CharField(max_length=100)
+    pub_date = models.DateField('date published')
+    chap_num = models.IntegerField(max_length=1000)
+    # tags = 
+    # author = 
+    # text = 
+    def __str__(self) -> str:
+        return self.topic
+    
