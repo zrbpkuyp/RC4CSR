0d68532fb3ad6b4576fd810a10b6b2381b229e58
Zhao Haochen <jtnkc@icloud.com>
2023-01-18 20:40:15 +0800

    system: update comments

--- System/Discussion/views.py
+++ System/Discussion/views.py
@@ -5,13 +5,13 @@ from .models import DiscGroup, DiscRecord
 
 # Create your views here.
 
-@login_required(redirect_field_name="origin")
+# @login_required(redirect_field_name="origin")
 def index(request):
     latest_group_list = DiscGroup.objects.order_by('-found_time')
     context = {'latest_group_list': latest_group_list}
     return render(request, 'Discussion/index.html', context)
 
-@login_required(redirect_field_name="origin")
+# @login_required(redirect_field_name="origin")
 def detail(request, group_id):
     group = get_object_or_404(DiscGroup, pk=group_id)
     records = DiscRecord.objects.filter(belong_to=group)
