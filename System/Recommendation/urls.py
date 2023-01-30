dec0b1b2f4fd008ad6b97b609c58f4327f8ca757
Zhao Haochen <jtnkc@icloud.com>
2023-01-30 20:36:18 +0800

    system: multi-level choice (Book tag), Recommendation buildup

--- System/Recommendation/urls.py
+++ System/Recommendation/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    path('book/<int:book_id>', views.detailBook, name='detailBook'),
+    # path('topic/<int:topic_id>', views.detailTopic, name='detailTopic'),
 ]
--- System/Recommendation/views.py
+++ System/Recommendation/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
+from django.contrib.auth.models import User
+import datetime
 
-# Create your views here.
+from .models import Book
+from Account.models import PlatformUser
+
+@login_required(redirect_field_name='login')
+def index(request):
+    platform_user = PlatformUser.objects.get(uid=request.user)
+    
+    return render(request, 'Recommendation/index.html', locals())
+
+
+@login_required(redirect_field_name='login')
+def search(request):
+    platform_user = PlatformUser.objects.get(uid=request.user)
+    
+    return render(request, 'Recommendation/search.html', locals())
+
+
+@login_required(redirect_field_name='login')
+def detailBook(request, book_id):
+    platform_user = PlatformUser.objects.get(uid=request.user)
+    book = Book.objects.get(id=book_id)
+    return render(request, 'Recommendation/detailBook.html', locals())
+
+'''
+@login_required(redirect_field_name='login')
+def detailTopic(request, book_id):
+    platform_user = PlatformUser.objects.get(uid=request.user)
+    
+    return render(request, 'Recommendation/detailTopic.html', locals())
+'''
